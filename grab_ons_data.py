b8b64dc502ed3fbe75d948729c4aa7221dc9ee9c
aeturrell <a.e.turrell@gmail.com>
2023-02-21 11:57:39 +0000

    improving env settings after poetry 1.3.2

--- grab_ons_data.py
+++ grab_ons_data.py
@@ -15,13 +15,15 @@ def get_list_of_model_series_identifiers() -> list:
         list: The list of four letter codes (capitalised).
     """
     with open(Path("data/model_spec/ons_identifiers.txt")) as f:
-        lines = [x.strip('\n') for x in f.readlines()]
+        lines = [x.strip("\n") for x in f.readlines()]
     unique_list = list(set(lines))
-    logger.info(f"There are {len(unique_list)} unique ONS time series codes in the model.")
+    logger.info(
+        f"There are {len(unique_list)} unique ONS time series codes in the model."
+    )
     return unique_list
 
 
-def get_series_ids_and_datasets(series_identifiers: list)-> pd.DataFrame:
+def get_series_ids_and_datasets(series_identifiers: list) -> pd.DataFrame:
     """Match up a given list of series identifiers with their dataset IDs.
 
     Args:
@@ -34,9 +36,13 @@ def get_series_ids_and_datasets(series_identifiers: list)-> pd.DataFrame:
     for series_id in series_identifiers:
         data = requests.get(ROOT_URL + f"timeseries/{series_id}").json()
         dataset_id = data["items"][0]["description"]["datasetId"]
-        temp_df = pd.DataFrame.from_dict({"series_id": series_id, "dataset_id": dataset_id}, orient="index").T
+        temp_df = pd.DataFrame.from_dict(
+            {"series_id": series_id, "dataset_id": dataset_id}, orient="index"
+        ).T
         series_dataset_df = pd.concat([series_dataset_df, temp_df])
-    logger.info(f"Dataset ids for {len(series_dataset_df)} time series have been downloaded.")
+    logger.info(
+        f"Dataset ids for {len(series_dataset_df)} time series have been downloaded."
+    )
     return series_dataset_df
 
 
@@ -52,25 +58,29 @@ def get_an_ONS_time_series(dataset_id: str, timeseries_id: str) -> pd.DataFrame:
     """
     # This function grabs specified time series from the ONS API.
     api_endpoint = "https://api.ons.gov.uk/"
-    api_params = {
-    'dataset':dataset_id,
-    'timeseries':timeseries_id}
-    url = (api_endpoint + '/'.join([x+'/'+y for x, y in zip(api_params.keys(), api_params.values())][::-1])
-                        + '/data')
+    api_params = {"dataset": dataset_id, "timeseries": timeseries_id}
+    url = (
+        api_endpoint
+        + "/".join(
+            [x + "/" + y for x, y in zip(api_params.keys(), api_params.values())][::-1]
+        )
+        + "/data"
+    )
     data = requests.get(url).json()
-    title = data['description']['title']
+    title = data["description"]["title"]
     # check if monthly or quarterly from whichever list not empty
-    if data['months']!=[]:
+    if data["months"] != []:
         time_series_data = data["months"]
     else:
         time_series_data = data["quarters"]
     data_df = pd.DataFrame(pd.json_normalize(time_series_data))
     data_df["title"] = title
     logger.debug("Quarter-only hard-coded here")
-    data_df["date_string"] = (data_df["year"] + "-" + data_df["quarter"])
+    data_df["date_string"] = data_df["year"] + "-" + data_df["quarter"]
     data_df["datetime"] = pd.to_datetime(data_df["date_string"])
     data_df = clean_columns(data_df)
     return data_df
+
 
 # example use
 data = get_an_ONS_time_series("PN2", "ABJR")
